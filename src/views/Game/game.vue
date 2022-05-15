c1600249de734e35c26489f92a1178347a3a5056
ZhengKe <3119531802@qq.com>
2022-05-15 16:19:25 +0800

    fix: init

---
+++ src/views/Game/game.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Game :init-data="initData" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import Game from "@/component/Game";
+
+const initData = ref({
+  x: 4,
+  y: 3,
+  imgUrl: ["./src/assets/images/2.jpg"],
+});
+</script>
+
+<style scoped></style>
